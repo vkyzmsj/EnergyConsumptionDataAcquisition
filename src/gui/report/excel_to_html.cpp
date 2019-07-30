d25b34119cc059daf29663c81076d9da6739996f
yukaikai <vkyzmsj@gmail.com>
2019-07-30 10:00:44 +0800

    完成 excel 到 html 的转化

--- src/gui/report/excel_to_html.cpp
+++ src/gui/report/excel_to_html.cpp
@@ -14,20 +14,25 @@ void ExcelToHtml::ConvertExcel(const QString &excel_path)
         return;
     }
 
-    QAxWidget *excel = new QAxWidget("Excel.Application") ;
-    excel->setProperty("Visible", false);
-    QAxObject * workbooks = excel->querySubObject("WorkBooks");
-    workbooks->dynamicCall("Open (const QString&)", QString("%1").arg(excel_path));
-    QAxObject * workBook = excel->querySubObject("ActiveWorkBook");
+    ////
+    /// 使用COM接口时，需要注意使用路径时，需要使用绝对路径，而且需要符合windows的标准，必须使用“\\”而不是“\”
+    /// 当使用相对路径，默认路径是系统的“文档”文件夹
+    /// 同时注意路径不能太长
+    ///
+    QAxObject excel( "Excel.Application", nullptr);
+    excel.setProperty("Visible", false);
+    QAxObject * workbooks = excel.querySubObject("WorkBooks");
 
-    QString out_html_path = QString("%1/report_%2.html").arg(QApplication::applicationDirPath()).arg(file_info.baseName());
-    workBook->dynamicCall("SaveAs (const QString&, int)", QString("%1").arg(out_html_path), 44);
+    QString full_report_path = QString("%1").arg(file_info.absoluteFilePath());
+    workbooks->dynamicCall("Open (const QString&)", QString("%1").arg(QString(full_report_path).replace('/', '\\')));
+    QAxObject * workBook = excel.querySubObject("ActiveWorkBook");
+
+    QString out_html_path = QString("%1/%2.html").arg(QApplication::applicationDirPath()).arg(file_info.baseName());
+    workBook->dynamicCall("SaveAs (const QString&, int)", QString("%1").arg(out_html_path).replace('/', '\\'), 44);
+//    workBook->dynamicCall("SaveAs (const QString&, int)", QString("QApplication::applicationDirPath()/%1").arg("sss.html"), 44);
 
     workBook->dynamicCall("Close (Boolean)", false);
-    excel->dynamicCall("Quit (void)");
-
-    delete workBook;
-    delete excel;
+    excel.dynamicCall("Quit (void)");
 
     emit ConvertFinish(out_html_path);
 }
